aab0329ea586181a2c231ade28d3f39b7d0359e3
Titouan Van Belle <titouan.vanbelle@orange.fr>
2015-12-20 17:13:45 +0100

    Typo

--- lib/spices/helpers/status_bar.rb
+++ lib/spices/helpers/status_bar.rb
@@ -1,5 +1,5 @@
 class StatusBar
   def self.visible?
-    Spices.world..uia('UIATarget.localTarget().frontMostApp().statusBar().isVisible()')['value'] == 1
+    Spices.world.uia('UIATarget.localTarget().frontMostApp().statusBar().isVisible()')['value'] == 1
   end
 end
