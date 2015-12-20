dc79045d4a73d794da0a7292dc2b701a8f20f62b
Titouan Van Belle <titouan.vanbelle@orange.fr>
2015-12-20 15:38:13 +0100

    Timeout=15

--- lib/spices/helpers/wait.rb
+++ lib/spices/helpers/wait.rb
@@ -1,5 +1,5 @@
 class Wait
-  def self.for(timeout, &_block)
+  def self.for(timeout=15, &_block)
     Spices.world.wait_for(timeout: timeout, retry_frequency: 0.5, screenshot_on_error: true) do
       yield
     end
