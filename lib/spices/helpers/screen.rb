9a41a1712c46a10286dcd885b870cd4d26889a02
Titouan Van Belle <titouan.vanbelle@orange.fr>
2015-12-20 13:30:52 +0100

    Orientation

--- lib/spices/helpers/screen.rb
+++ lib/spices/helpers/screen.rb
@@ -22,4 +22,28 @@ class Screen
   def self.width_scaled
     width / scale
   end
+
+  def self.home_direction
+    Spices.world.home_direction
+  end
+
+  def self.portrait?
+    home_direction == :down || home_direction == :up
+  end
+
+  def self.landscape?
+    home_direction == :left || home_direction == :right
+  end
+
+  def self.rotate(dir)
+    Spices.world.rotate dir
+  end
+
+  def self.rotate_to_landscape
+    Spices.world.rotate_home_button_to :left
+  end
+
+  def self.rotate_to_portrait
+    Spices.world.rotate_home_button_to :down
+  end
 end
