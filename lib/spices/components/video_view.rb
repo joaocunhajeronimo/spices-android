7523098fc7299aa5c52219184daebd385d019c6b
Joao Jeronimo <joao.jeronimo@fyber.com>
2016-01-25 17:37:37 +0100

    fixing view_view

--- lib/spices/components/video_view.rb
+++ lib/spices/components/video_view.rb
@@ -1,15 +1,17 @@
-class VideoView < View
-  def type
-    :videoView
-  end
-
+class VideoView < BaseComponent
   def playing?
     self['query', 'isPlaying']
   end
 
   def duration
-    duration = Spices.world.query('videoView', 'getDuration')[0]
+    duration = Spices.world.query(query_string, 'getDuration')[0]
     # self query does not seem to work for 'getDuration': self['query', 'getDuration']
     duration / 1000
+  end
+
+  private
+
+  def type
+    :videoView
   end
 end
