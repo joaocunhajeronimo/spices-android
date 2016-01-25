2788f549f90c128d2dddc769442b62f42522dd68
Joao Jeronimo <joao.jeronimo@fyber.com>
2016-01-25 12:19:44 +0100

    added videoView component

---
+++ lib/spices/components/video_view.rb
@@ -0,0 +1,15 @@
+class VideoView < View
+  def type
+    :videoView
+  end
+
+  def playing?
+    self['query', 'isPlaying']
+  end
+
+  def duration
+    duration = video_view = Spices.world.query('videoView', 'getDuration')[0]
+    # self query does not seem to work for 'getDuration': self['query', 'getDuration']
+    duration/1000
+  end
+end
