54198b88b87eca74f87769c710a986dd2be26e0a
Joao Jeronimo <joao.jeronimo@fyber.com>
2016-01-25 12:20:19 +0100

    brought new wait methods from iOS spices

--- lib/spices/helpers/wait.rb
+++ lib/spices/helpers/wait.rb
@@ -5,8 +5,24 @@ class Wait
     end
   end
 
-  def self.for_ui_animation_to_end
-    Spices.world.wait_for_none_animating(timeout: 10, screenshot_on_error: false)
-  rescue
+  def self.for_not_to_happen(timeout_value, &_block)
+    retry_frequency = 0.1
+
+    begin
+      Timeout.timeout(timeout_value, TimeoutError) do
+        sleep retry_frequency until yield
+        fail 'Something that was not supposed to happen happened' if yield
+      end
+    rescue TimeoutError
+      'Something that was not supposed to happen did not happen. All good'
+    rescue => e
+      Spices.world.screenshot_and_raise e
+    end
+  end
+
+  def self.for_and_do(test, &_block)
+    Spices.world.wait_poll(timeout: 15, timeout_message: '', until: test) do
+      yield
+    end
   end
 end
