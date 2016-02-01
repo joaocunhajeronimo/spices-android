3d1bcaddbea439dabf897c9d7dd60cb9374cef47
Mofazzal Hossain <mhossain@Ronys-MacBook-Pro.local>
2016-02-01 16:25:36 +0100

    Refactored and removed the extra lines

--- lib/spices/components/video_view.rb
+++ lib/spices/components/video_view.rb
@@ -1,12 +1,9 @@
 class VideoView < BaseComponent
   def playing?
-    # Spices.world.query(query_string, 'isPlaying')[0]
     self['isPlaying']
   end
 
   def duration
-    # duration = Spices.world.query(query_string, 'getDuration')[0]
-    # self query does not seem to work for 'getDuration': self['query', 'getDuration']
     self['getDuration'] / 1000
   end
 
