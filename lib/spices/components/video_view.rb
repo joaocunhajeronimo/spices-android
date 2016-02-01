2e79452603d2d39dcf94312cfa7b5f65d1abd107
Mofazzal Hossain <mhossain@Ronys-MacBook-Pro.local>
2016-02-01 16:20:13 +0100

    Typo:duratoin should be getDuration

--- lib/spices/components/video_view.rb
+++ lib/spices/components/video_view.rb
@@ -7,7 +7,7 @@ class VideoView < BaseComponent
   def duration
     # duration = Spices.world.query(query_string, 'getDuration')[0]
     # self query does not seem to work for 'getDuration': self['query', 'getDuration']
-    self['duration'] / 1000
+    self['getDuration'] / 1000
   end
 
   private
